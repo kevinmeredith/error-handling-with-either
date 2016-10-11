64edcbde2a725610b01572c4db0836afbde4bd65
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-10-10 22:22:31 -0400

    first

---
+++ src/main/scala/net/service/OrderService.scala
@@ -0,0 +1,27 @@
+package net.service
+
+import java.sql.SQLException
+
+import net.http.HttpOrderService.OrderRequest
+
+object OrderService {
+
+	sealed trait UpdateOrderError
+	case object NoOrderFound               extends UpdateOrderError
+	case class DbUpdateError(t: Throwable) extends UpdateOrderError
+
+	// An Order that exists in the system, i.e. DB
+	case class Order(id: Long, quantity: Int)
+
+	def update(req: OrderRequest): Either[UpdateOrderError, Unit] = 
+		get(req) match {
+			case Right(order) => updateHelper(order)
+			case Left(e)      => Left(e)
+		}
+
+	private def updateHelper(o: Order): Either[UpdateOrderError, Unit] = 
+		Left(DbUpdateError(new SQLException("Constraint XYZ violated.")))
+
+	private def get(o: OrderRequest): Either[UpdateOrderError, Order] = 
+		???
+}
